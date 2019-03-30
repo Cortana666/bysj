2b1779bf24e536ac7a0944f24965195f02bab2d8
catindog <jian635446559@gmail.com>
2019-03-30 16:06:54 +0800

    框架搭建

---
+++ application/views/templates_c/044f6f414af9eb5cbef194986ee9b33da9fa8316_0.file.welcome_message.html.php
@@ -0,0 +1,28 @@
+<?php
+/* Smarty version 3.1.33, created on 2019-03-30 07:36:45
+  from 'C:\Users\nginx-1.14.2\html\application\views\templates\welcome_message.html' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.33',
+  'unifunc' => 'content_5c9f1c8de03960_81124809',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '044f6f414af9eb5cbef194986ee9b33da9fa8316' => 
+    array (
+      0 => 'C:\\Users\\nginx-1.14.2\\html\\application\\views\\templates\\welcome_message.html',
+      1 => 1553931305,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5c9f1c8de03960_81124809 (Smarty_Internal_Template $_smarty_tpl) {
+?><html>
+	<?php echo $_smarty_tpl->tpl_vars['aaa']->value;?>
+
+</html><?php }
+}
